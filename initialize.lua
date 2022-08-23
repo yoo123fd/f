24622f1158a772f7cbaad17b4dcb0e9fec5e7b6f
xz23 <newval123@protonmail.com>
2022-08-23 15:35:10 -0400

    a

--- initialize.lua
+++ initialize.lua
@@ -1,1 +1,15 @@
+local Players = game:GetService("Players")
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+getgenv().Variables = {
+    Players = game:GetService("Players"),
+    ReplicatedStorage = game:GetService("ReplicatedStorage"),
+    Client = Players.LocalPlayer,
+    Character = Players.LocalPlayer.Client.Character or Players.LocalPlayer.CharacterAdded:Wait()
+}
 
+Variables.Client.CharacterAdded:Connect(function(Character)
+    Variables.Character = Character 
+end)
+
+
+local success
--- loader.lua
+++ loader.lua
@@ -1,1 +1,6 @@
+local success, result = pcall(game.HttpGet, game, "https://raw.githubusercontent.com/yoo123fd/ff2/main/initialize.lua")
+
+if success then
+    loadstring(result)()
+end
 
