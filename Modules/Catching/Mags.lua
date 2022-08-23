30a80324fa8f234468167dcba0b2c08efcbd5b5e
xz23 <newval123@protonmail.com>
2022-08-23 17:59:30 -0400

    a

--- Modules/Catching/Mags.lua
+++ Modules/Catching/Mags.lua
@@ -7,14 +7,15 @@ do
     local MagSection = Variables.CatchingTab:AddSection("Mags")
     local place0 = MagSection:AddToggle("Enabled", {flag = "Mags_Enabled"}, function() end)
     local place1 = MagSection:AddSlider("Distance", 1, 40, 8, {flag = "Mags_Distance"}, function() end) 
-    local place = MagSection:AddBind("Toggle key", Enum.KeyCode.Q, {flag = "Mags_ToggleKey"}, function()
+    local place = MagSection:AddBind("Toggle key", Enum.KeyCode.Q, {flag = "Mags_ToggleKey"}, function() 
+        print('ye')
         LastChanged = tick()
     end)
 
     Variables.UserInputService.InputBegan:Connect(function(input, gameProcessedEvent)
         if not gameProcessedEvent then
             if input.KeyCode == place.Bind and tick() - LastChanged >= .3 then
-                place0:Set(not Variables.CatchingTab.Flags["Mags_Enabled"], false)
+                place0:Set(not Variables.CatchingTab.Flags["Mags_Enabled"], true)
             end
         end
     end)
