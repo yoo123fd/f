8dd972169807929fab02c9a78b5d775bf217cd49
xz23 <newval123@protonmail.com>
2022-08-23 16:05:09 -0400

    a

--- loader.lua
+++ loader.lua
@@ -1,6 +1,7 @@
-warn('called')
 local success, result = pcall(game.HttpGet, game, "https://raw.githubusercontent.com/yoo123fd/f/main/initialize.lua")
 if success then
     loadstring(result)()
+else
+    game:GetService("Players").LocalPlayer:Kick("There was an error attempting to load: Football Fusion 2 | " .. tostring(result))
 end
 
