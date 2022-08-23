0e0e0203b682310f37fd1e671b511200d179ea67
xz23 <newval123@protonmail.com>
2022-08-23 15:52:47 -0400

    A

--- loader.lua
+++ loader.lua
@@ -1,5 +1,5 @@
 local success, result = pcall(game.HttpGet, game, "https://raw.githubusercontent.com/yoo123fd/ff2/main/initialize.lua")
-
+warn(success)
 if success then
     loadstring(result)()
 end
