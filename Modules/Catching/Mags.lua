c587271b985ab8c93cc02f159719460f51754a41
xz23 <newval123@protonmail.com>
2022-08-23 16:31:36 -0400

    a

--- Modules/Catching/Mags.lua
+++ Modules/Catching/Mags.lua
@@ -1,4 +1,12 @@
 local Mags = {}
 
+--// UI Setup
+do
+    local MagSection = Variables.CatchingTab:AddSection("Mags")
+    MagSection:AddToggle("Enabled", {flag = "Mags_Enabled"}, function() end)
+    MagSection:AddSlider("Distance", 1, 40, 8, {flag = "Mags_Distance"}, function() end) 
+end
+
+
 
 return Mags 
